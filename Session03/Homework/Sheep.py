ae8aafb62701e10448a4a1c665fd31c64877029e
Duong Tuan <duongqtuan3008@gmail.com>
2019-02-07 21:28:38 +0700

    .

---
+++ Session03/Homework/Sheep.py
@@ -0,0 +1,23 @@
+print("Hello, my name is Tuấn and these are my sheep sizes")
+sheep = [5,7,300,90,24,50,75]
+print(sheep)
+print()
+sum = 0
+for m in range(4):
+    print("MONTH",m+1)
+    print("Now my biggest sheep has size",max(sheep),"let's shear it")
+    print()
+    sum += max(sheep)
+    del sheep[sheep.index(max(sheep))]
+    print("After shearing, here's my flock")
+    print(sheep)
+    print() 
+    print("One month has passed, now here is my flock")
+    sheep1=[]
+    for i in sheep:
+        i += 50
+        sheep1.append(i)
+    print(sheep1)
+    sheep = sheep1
+print()
+print("I have earn ",sum,"$*2 = ",sum*2,"$",sep='')
